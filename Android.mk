df8724cc5803754643e718071e749b35c6a6ffc2
Daniel Micay <danielmicay@gmail.com>
2022-01-17 18:27:09 -0500

    initial commit

---
+++ Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := Apps
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := prebuilt/Apps.apk
+
+include $(BUILD_PREBUILT)
